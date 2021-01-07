e2a1b551eabe5f753c94adf512ea47c451df4cda
ChiefGains <48441498+ChiefGains@users.noreply.github.com>
2021-01-07 14:47:34 -0500

    Update lingress_update.go
    Updated fitLine to (mostly) work with multiple regression, though I'm still unclear on how calculating the slope works

--- linearAlgebra/regression/lingress_update.go
+++ linearAlgebra/regression/lingress_update.go
@@ -1,4 +1,4 @@
-/*Package regression Version 0.1 of my regression package. So far just does some very basic linear
+/*Package regression Version 0.2 of my regression package. So far just does some very basic linear
 regression,and isn't even able to auto-fill data. Eventually, I'll be able to load a CSV and take a
 column of the dataframe, and temporarily convert it into a line for use in this package. Hopefully
 this package will also eventually be able to work in tandem with the Matrices package. Would also
@@ -57,23 +57,23 @@ func (l *Line) randFill(limit int) {
 		l.Y[i] = float64(rand.Intn(limit))
 	}
 
-	var yHat float64 //I should probably add xHat and yHat to the line struct, but that sounds like a headache
-	yHat = 0
+	var ySum float64 //I should probably add xSum and ySum to the line struct, but that sounds like a headache
+	ySum = 0
 
 	for i := range l.X {
-		var xHat float64
-		xHat = 0
+		var xSum float64
+		xSum = 0
 
 		for j := range l.X[i] {
-			xHat += l.X[i][j]
+			xSum += l.X[i][j]
 		}
 
-		l.XAvg[i] = xHat / l.Length
+		l.XAvg[i] = xSum / l.Length
 
-		yHat += l.Y[i]
+		ySum += l.Y[i]
 	}
 
-	l.YAvg = yHat / l.Length
+	l.YAvg = ySum / l.Length
 }
 
 /*fitLine like *super* needs updated to handle multiple regression. Should
@@ -81,36 +81,36 @@ be as simple as an iterative statement going through and doing least squares
 for each dimension*/
 
 //fitLine takes data points and fits them to a regression line
-func (l *Line) fitLine() *Line {
-	var num float64
-	var den float64
-
-	num, den = 0, 0
-
+func (l *Line) fitLine() {
 	for i := range l.X {
-		//for j := range l.X[i]
-		//I'll fix this later, I'm too tired now
-		a := l.X[i] - l.XAvg
-		b := l.Y[i] - l.YAvg
+		var num float64
+		var den float64
 
-		num += a * b
-		den += math.Pow(a, 2)
+		num, den = 0, 0
+
+		for j := range l.X {
+			a := l.X[i][j] - l.XAvg[i]
+			b := l.Y[j] - l.YAvg
+
+			num += a * b
+			den += math.Pow(a, 2)
+		}
+
+		l.Slope[i] = num / den
 	}
 
-	l.Slope = num / den
+	//l.Intercept = l.YAvg - (l.Slope * l.XAvg) *I DON'T KNOW HOW TO DO THIS FOR MULTIPLE REGRESSION*
 
-	l.Intercept = l.YAvg - (l.Slope * l.XAvg)
-
-	return l
 }
 
+/*predict still needs updated to handle multiple, don't know how to do that yet*/
 //predict looks at a fit line, looks at an input variable, and predicts the outcome based on the fit line
-func (l *Line) predict(i int) *Line {
+func (l *Line) predict(i int) float64 {
 	x := float64(i)
 
 	y := l.Intercept + (l.Slope * x)
 
 	fmt.Println(y)
 
-	return l
+	return y
 }
